ed74463c47cf004255bad91baef1be5f6e008e29
SudhanshuChoursiya <sudhanshuchoursiya2@gmail.com>
2022-08-12 13:41:28 +0530

    modified

--- public/frontend.js
+++ public/frontend.js
@@ -1,5 +1,8 @@
 let alert_box=document.querySelector('.alert_box');
 
+let input_value=document.getElementById('inputvalue');
+
+
 function alerts() {
     alert_box.style.display='none'    
 }
@@ -7,3 +10,11 @@ function alerts() {
 setTimeout(alerts,5000);
 
 
+function deletepopup() {
+
+   if (confirm("are you sure you want to delete")==false) {
+       
+       input_value.setAttribute('value','');
+   }
+    
+}
